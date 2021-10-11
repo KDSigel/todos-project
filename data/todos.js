b2322d1d289bb0203b917e4a233132238e4bddda
Karl Sigel <heykarl@gmail.com>
2021-10-11 14:01:05 -0700

    added some base stuff

---
+++ data/todos.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    todo: 'eat dinner',
+    completed: false,
+  },
+  {
+    todo: 'do career services assignment',
+    completed: false,
+  },
+  {
+    todo: 'read',
+    completed: false,
+  }
+];
